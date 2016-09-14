9dc1f65c19530ef42dabb279ccfc8b98e2920821
Gilang Gautama <gau_1st@yahoo.co.id>
2016-09-15 05:02:20 +0700

    Fix the structure

--- gulpfile.js
+++ gulpfile.js
@@ -40,15 +40,15 @@ gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
  * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
  */
 gulp.task('sass', function () {
-    return gulp.src('_scss/main.scss')
+    return gulp.src('assets/css/main.sass')
         .pipe(sass({
-            includePaths: ['scss'],
+            includePaths: ['assets/css/'],
             onError: browserSync.notify
         }))
         .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-        .pipe(gulp.dest('_site/css'))
+        .pipe(gulp.dest('assets/css/'))
         .pipe(browserSync.reload({stream:true}))
-        .pipe(gulp.dest('css'));
+        .pipe(gulp.dest('_site/css'));
 });
 
 /**
@@ -56,7 +56,11 @@ gulp.task('sass', function () {
  * Watch html/md files, run jekyll & reload BrowserSync
  */
 gulp.task('watch', function () {
-    gulp.watch('_scss/*.scss', ['sass']);
+    gulp.watch('assets/css/*.sass', ['sass']);
+    gulp.watch('assets/css/1-tools/*.*', ['sass']);
+    gulp.watch('assets/css/2-basics/*.*', ['sass']);
+    gulp.watch('assets/css/3-modules/*.*', ['sass']);
+    gulp.watch('assets/css/4-pages/*.*', ['sass']);
     gulp.watch(['*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
 });
 
